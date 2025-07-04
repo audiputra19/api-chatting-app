a5d27eeb71710ff03e73bfd8ecf4a209500d58dc
audi putra <audiputra19@gmail.com>
2025-07-04 14:50:08 +0700

    update app.ts and vercel.json

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express, { Request, Response } from "express";
+import contactRouter from "./routers/contactRouter";
+import cors from "cors";
+import registerRouter from './routers/registerRouter';
+import userRouter from './routers/userRouter';
+import http from "http";
+import { Server } from "socket.io";
+import { registerSocketHandlers } from './socket/socket';
+import chatRouter from './routers/chatRouter';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
+
+app.use(express.json());
+app.use(cors())
+app.use('/', contactRouter);
+app.use('/', registerRouter);
+app.use('/', userRouter);
+app.use('/', chatRouter);
+
+registerSocketHandlers(io);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Express + TypeScript on Vercel!");
+});
